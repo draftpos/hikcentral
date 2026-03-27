b02f520bd76971d7fa07813ffccc035ae818c93b
makonia20 <makonia20@gmail.com>
2026-03-27 12:50:30 +0200

    Initial commit: Hikcentral Frappe app with attendance DocType and fields

---
+++ hikcentral/hikcentral/doctype/hik_attendance_log/hik_attendance_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ashley and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Hik Attendance Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ hikcentral/hikcentral/doctype/hik_attendance_log/test_hik_attendance_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ashley and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHikAttendanceLog(FrappeTestCase):
+	pass
